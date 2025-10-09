f7926cc248d10c818f33f503eebed404e22d7b79
TriSuccess <73508202+TriSuccess@users.noreply.github.com>
2025-10-09 01:39:55 +0100

    Your commit message here

--- app/api/secure-video/route.ts
+++ app/api/secure-video/route.ts
@@ -14,7 +14,7 @@ const corsHeaders = {
 const certObj = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  privateKey: process.env.FIREBASE_SERVICE_ACCOUNT_KEY?.replace(/\\n/g, "\n"),
 };
 
 if (!getApps().length) {
