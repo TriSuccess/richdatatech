ffd7c41c844a3a568f22c1e85fde690cf43f0146
TriSuccess <73508202+TriSuccess@users.noreply.github.com>
2025-10-11 21:19:01 +0100

    Your commit message here

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add more paths if you have other folders with Tailwind classes
+  ],
+  darkMode: "class", // or 'media' or false, as you prefer
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
